f2b065388ad00cb1606997c83789963523ab1c41
krazykyran <kyran.mcg@gmail.com>
2017-09-07 15:18:00 +0930

    implementation of MainClass and event handler, simple qtConsole app.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
-#include <QtCore>
+#include <QCoreApplication>
+#include <QTimer>
+
+#include "mainclass.h"
+
+MainClass *mainTask;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    // create the main class
+    mainTask = new MainClass(&a);
+
+    // connect up the signals
+    QObject::connect(mainTask, SIGNAL(finished()), &a, SLOT(quit()));
+    QObject::connect(&a, SIGNAL(aboutToQuit()), mainTask, SLOT(aboutToQuitApp()));
+
+    // create singleshot timer to start run() in mainTask after 100ms
+    QTimer::singleShot(100, mainTask, SLOT(run()));
 
     return a.exec();
 }
--- qt-console.pro
+++ qt-console.pro
@@ -9,4 +9,8 @@ CONFIG -= app_bundle
 
 TEMPLATE = app
 
-SOURCES += main.cpp
+SOURCES += main.cpp \
+    mainclass.cpp
+
+HEADERS += \
+    mainclass.h
